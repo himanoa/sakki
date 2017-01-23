c831ecc666efdc8a82462b342f261768bb71ad0a
himanoa <matsunoappy@gmail.com>
2017-01-23 15:10:17 +0900

    add layout.slim

--- repositories/entry_repositoriy.rb
+++ repositories/entry_repositoriy.rb
@@ -1,12 +1,17 @@
 class EntryRepository
+  include Enumerable
   def initialize
     @entries = []
     @entries.length - 1
   end
   def save(entry)
     @entries.push(entry)
+    return @entries.length -1
   end
   def fetch(id)
     @entries[id]
+  end
+  def each(&block)
+    @entries.each(&block)
   end
 end

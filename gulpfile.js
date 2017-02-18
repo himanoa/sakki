7a15b2a65ce053438f8f8ac0d2f8f34d69aee4d5
himanoa <matsunoappy@gmail.com>
2017-02-18 09:21:37 +0900

    activate modan web frontend develop environment

--- gulpfile.js
+++ gulpfile.js
@@ -4,6 +4,9 @@ const postcss      = require('gulp-postcss');
 const sourcemaps   = require('gulp-sourcemaps');
 const autoprefixer = require('autoprefixer');
 const process = require('gulp-process');
+const browserify = require('browserify');
+const babelify = require('babelify');
+const source = require('vinyl-source-stream');
 
 gulp.task('sass', function () {
   return gulp.src('./sass/**/*.scss')
@@ -18,6 +21,20 @@ gulp.task('sass:watch', function () {
   gulp.watch('./sass/**/*.scss', ['sass']);
 });
 
+gulp.task('babelify', function(){
+  browserify({
+    entries: './js/app.js',
+    extensions: ['.js']
+  })
+  .transform(babelify)
+  .bundle()
+  .on("error", function(err){
+    console.error(err);
+    this.emit("end");
+  })
+  .pipe(source("app.js"))
+  .pipe(gulp.dest("./public/js"))
+})
 
 gulp.task('build', ['sass']);
 
--- js/app.js
+++ js/app.js
@@ -1,3 +1,6 @@
+const Vue = require('../node_modules/vue/dist/vue.min.js');
+const marked = require('marked');
+const _ = require('lodash');
 document.addEventListener("DOMContentLoaded", function(){
   if(!document.querySelector('#editor')){
     return ;
